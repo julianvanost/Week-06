a6e1515cc0a3fd86f7cdb6b2b865cdb51630ea08
Julian <Van Ost>
2020-02-19 13:20:31 -0800

    Adds HotRestaurant application to week 6

--- Activities/HotRestaurant/Solved/.history/server_20200219130213.js
+++ Activities/HotRestaurant/Solved/.history/server_20200219130213.js
@@ -14,7 +14,7 @@ var express = require("express");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8081;
+var PORT = process.env.PORT || 6666;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -34,6 +34,6 @@ require("./routes/htmlRoutes")(app);
 // The below code effectively "starts" our server
 // =============================================================================
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
--- Activities/HotRestaurant/Solved/.history/server_20200219130219.js
+++ Activities/HotRestaurant/Solved/.history/server_20200219130219.js
@@ -14,7 +14,7 @@ var express = require("express");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8081;
+var PORT = process.env.PORT || 6666;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
